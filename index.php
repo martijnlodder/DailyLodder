41824af3f54e4fbed85156b9045653b5c86d9926
martijnlodder <thesevenprovinces@gmail.com>
2019-12-27 17:58:48 +0100

    readded them

---
+++ index.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html lang="nl">
+<head>
+  <meta charset="utf-8">
+  <link rel="stylesheet" href="css/style.css"/>
+  <title>Daily Lodder - Home</title>
+</head>
+<body>
+  <h1>Daily Lodder</h1>
+</body>
+</html>
