8e9ef8dab93ead2c03f19ea19b17b02505cad3f1
martijnlodder <thesevenprovinces@gmail.com>
2019-12-30 17:46:45 +0100

    created a title, nav, and some php code

--- index.php
+++ index.php
@@ -6,6 +6,28 @@
   <title>Daily Lodder - Home</title>
 </head>
 <body>
-  <h1>Daily Lodder</h1>
+  <header>
+    <div class="headertitle">
+      <h1>Daily Lodder</h1>
+      <br>
+    </header>
+    <nav>
+      <a href="index.php">Home</a>
+      <a href="informationsecurity.php">Info-Sec</a>
+      <a href="howto.php">How To's</a>
+      <a href="linux.php">Linux</a>
+    </nav>
+    <section>
+      <p>Under Construction</p>
+    </section>
+    <article>
+      <p>Article Under Construction</p>
+    </article>
+    <footer>
+      <?php
+      include 'php/date.php';
+      ?>
+    </footer>
+  </div>
 </body>
 </html>
