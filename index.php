dea907064ba27517631cad525ae9f42cf68405e0
Martijn Lodder <58008304+martijnlodder@users.noreply.github.com>
2020-01-26 16:54:10 +0100

    New version of Daily Lodder

---
+++ index.php
@@ -0,0 +1,53 @@
+<!doctype html>
+<html lang="nl">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="css/style.css"/>
+    <title>Daily Lodder | Home</title>
+</head>
+<body>
+    <header class="main-header">
+        <div class="navigation-container">
+            <div class="title">
+                <h2>Daily Lodder</h2>
+            </div>
+            <div class="main-navigation">
+                <ul>
+                    <li><a href="index.php">home &nbsp;|&nbsp;</a></li>
+                    <li><a href="tech.php">tech &nbsp;|&nbsp;</a></li>
+                    <li><a href="aboutus.php">about us &nbsp;|&nbsp;</a></li>
+                    <li><a href="contact.php">contact &nbsp;|&nbsp;</a></li>
+                    <li><a href="login.php">login</a></li>
+                </ul>
+            </div>
+        </div>
+    </header>
+
+    <section id="homepage-message">
+        <div class="container">
+            <h3 class="titlecenter">
+                Daily Lodder Tech<br><span class="bold">News<span class="trademark">™</span></span>
+            </h3>
+        </div>
+        
+        <section class="video">
+            <video class="video-background" src="vid/mainvid.mp4" autoplay muted loop style="width: auto; height: 100%;"></video>
+        </section>
+
+        <footer>
+            <br>
+            <a href="https://twitter.com/7Provi" target="_blank">
+            <img src="afb/twitter.svg" class="social-link"></a>
+            <br>
+            <br>
+            ©2020 Daily Lodder - All Rights Reserved. Privacy | Terms
+            <br>
+            <br>
+        </footer>
+</body>
+</html>
+
+<!-- Deze komt in Tech
+        <a href="informationsecurity.php">Info-Sec</a>
+        <a href="howto.php">How To's</a>
+        <a href="linux.php">Linux</a> -->
